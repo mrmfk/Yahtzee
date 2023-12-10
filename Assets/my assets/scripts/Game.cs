fbdb03e84506e235c1bdc51ba4212bd992a8c0a9
mrmfk <50992240+mrmfk@users.noreply.github.com>
2023-12-11 02:58:30 +0330

    game logic

--- Assets/my assets/scripts/Game.cs
+++ Assets/my assets/scripts/Game.cs
@@ -8,7 +8,7 @@ public class Game : MonoBehaviour
 
     void Start()
     {
-      
+        // ‘—Ê⁄ »«“? »«  ⁄œ«œ »«“?ò‰«‰ „‘Œ’
         StartGame(2);
     }
 
@@ -30,19 +30,81 @@ public class Game : MonoBehaviour
 
         foreach (Player player in players)
         {
-            player.RollDice(FindObjectsOfType<Dice>());
+            Dice[] dice = FindObjectsOfType<Dice>();
+            player.RollDice(dice);
+
+            // ‰„«?‘ ‰ «?Ã Å” «“ Å— «»  «”ùÂ«
+            Debug.Log(player.playerName + "'s Dice Values: " + GetDiceValues(dice));
+            Debug.Log(player.playerName + "'s Scoresheet:");
+            DisplayScoreSheet(player);
         }
+
+        // „—Õ·Â «„ ?«“œÂ?
+        ScoreRound();
     }
 
-    public void EndRound()
+    public void ScoreRound()
     {
         foreach (Player player in players)
         {
- 
-            player.CalculateTotalScore();
+            // «‰ Œ«» œ” Âù? «„ ?«“œÂ?
+            int selectedCategory = GetSelectedCategoryFromPlayer(player);
+
+            // «„ ?«“œÂ? »Â »«“?ò‰ »— «”«” œ” Âù? «‰ Œ«» ‘œÂ
+            player.ScoreInCategory(selectedCategory, GetDiceValues(FindObjectsOfType<Dice>()));
         }
 
+        // « „«„ œÊ— Ê «œ«„Â »Â œÊ— »⁄œ? ?« Å«?«‰ »«“?...
         currentRound++;
-        StartRound();
+        if (currentRound <= 13) // 13 œÊ— œ— »«“? Yahtzee
+        {
+            StartRound();
+        }
+        else
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        // «‰Ã«„ ⁄„·?«  Å«?«‰ »«“?
+        Debug.Log("Game Over");
+
+        // ‰„«?‘ ‰ «?Ã ‰Â«??
+        foreach (Player player in players)
+        {
+            Debug.Log(player.playerName + "'s Final Score: " + player.totalScore);
+        }
+    }
+
+    // «‰ Œ«» œ” Âù? «„ ?«“œÂ? «“ »«“?ò‰
+    public int GetSelectedCategoryFromPlayer(Player player)
+    {
+        // «?‰Ã« »«?œ òœ? ‰Ê‘  òÂ «“ »«“?ò‰ »ŒÊ«Âœ ?ò œ” Â «‰ Œ«» ò‰œ.
+        // „À·« ?ò UI ‰„«?‘ œÂ?œ Ê »«“?ò‰ »ŒÊ«Âœ ?ò ê“?‰Â —« «‰ Œ«» ò‰œ.
+        // »—«? ”«œê?° «?‰Ã« «“ ?ò ⁄œœ  ’«œ›? »?‰ 1  « 13 «” ›«œÂ ‘œÂ «” .
+        return Random.Range(1, 14);
+    }
+
+    // ‰„«?‘ ‰ «?Ã «„ ?«“œÂ? »—«? ?ò »«“?ò‰
+    void DisplayScoreSheet(Player player)
+    {
+        foreach (int score in player.scores)
+        {
+            Debug.Log(score);
+        }
+        Debug.Log("Total Score: " + player.totalScore);
+    }
+
+    // ê—› ‰ „ﬁ«œ?—  «”ùÂ«
+    int[] GetDiceValues(Dice[] dice)
+    {
+        int[] values = new int[dice.Length];
+        for (int i = 0; i < dice.Length; i++)
+        {
+            values[i] = dice[i].faceValue;
+        }
+        return values;
     }
 }
--- Assets/my assets/scripts/Player.cs
+++ Assets/my assets/scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -40,6 +41,153 @@ public class Player : MonoBehaviour
         {
             totalScore += score;
         }
+    }
+
+
+    public void ScoreInCategory(int category, int[] diceValues)
+    {
+        int score = 0;
+
+        switch (category)
+        {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                score = CountDiceWithValue(diceValues, category);
+                break;
+            case 7: // Three of a Kind
+                score = GetNOfAKindScore(diceValues, 3);
+                break;
+            case 8: // Four of a Kind
+                score = GetNOfAKindScore(diceValues, 4);
+                break;
+            case 9: // Full House
+                score = GetFullHouseScore(diceValues);
+                break;
+            case 10: // Small Straight
+                score = GetSmallStraightScore(diceValues);
+                break;
+            case 11: // Large Straight
+                score = GetLargeStraightScore(diceValues);
+                break;
+            case 12: // Yahtzee
+                score = GetYahtzeeScore(diceValues);
+                break;
+            case 13: // Chance
+                score = GetChanceScore(diceValues);
+                break;
+        }
+
+        scores.Add(score);
+    }
+
+    //  ⁄œ«œ  «”ùÂ«?? òÂ œ«—«? ?ò „ﬁœ«— Œ«’ Â” ‰œ —« »—„?ùê—œ«‰œ
+    private int CountDiceWithValue(int[] diceValues, int value)
+    {
+        int count = 0;
+        foreach (int diceValue in diceValues)
+        {
+            if (diceValue == value)
+            {
+                count++;
+            }
+        }
+        return count * value;
+    }
+
+    // «„ ?«“ ?ò œ” Â N «“ ?ò ‰Ê⁄ („À· Three of a Kind ?« Four of a Kind)
+    private int GetNOfAKindScore(int[] diceValues, int n)
+    {
+        Array.Sort(diceValues);
+        for (int i = 0; i <= diceValues.Length - n; i++)
+        {
+            if (diceValues[i] == diceValues[i + n - 1])
+            {
+                return SumDiceValues(diceValues);
+            }
+        }
+        return 0;
+    }
+
+    // «„ ?«“ Full House
+    private int GetFullHouseScore(int[] diceValues)
+    {
+        Array.Sort(diceValues);
+        if ((diceValues[0] == diceValues[1] && diceValues[2] == diceValues[4]) ||
+            (diceValues[0] == diceValues[2] && diceValues[3] == diceValues[4]))
+        {
+            return 25;
+        }
+        return 0;
+    }
+
+    // «„ ?«“ Small Straight
+    private int GetSmallStraightScore(int[] diceValues)
+    {
+        Array.Sort(diceValues);
+        for (int i = 0; i <= diceValues.Length - 4; i++)
+        {
+            if (diceValues[i] == diceValues[i + 1] - 1 &&
+                diceValues[i + 1] == diceValues[i + 2] - 1 &&
+                diceValues[i + 2] == diceValues[i + 3] - 1)
+            {
+                return 30;
+            }
+        }
+        return 0;
+    }
+
+    // «„ ?«“ Large Straight
+    private int GetLargeStraightScore(int[] diceValues)
+    {
+        Array.Sort(diceValues);
+        for (int i = 0; i <= diceValues.Length - 5; i++)
+        {
+            if (diceValues[i] == diceValues[i + 1] - 1 &&
+                diceValues[i + 1] == diceValues[i + 2] - 1 &&
+                diceValues[i + 2] == diceValues[i + 3] - 1 &&
+                diceValues[i + 3] == diceValues[i + 4] - 1)
+            {
+                return 40;
+            }
+        }
+        return 0;
+    }
+
+    // «„ ?«“ Yahtzee
+    private int GetYahtzeeScore(int[] diceValues)
+    {
+        for (int i = 0; i <= diceValues.Length - 5; i++)
+        {
+            if (diceValues[i] == diceValues[i + 1] &&
+                diceValues[i + 1] == diceValues[i + 2] &&
+                diceValues[i + 2] == diceValues[i + 3] &&
+                diceValues[i + 3] == diceValues[i + 4])
+            {
+                return 50;
+            }
+        }
+        return 0;
+    }
+
+    // «„ ?«“ Chance
+    private int GetChanceScore(int[] diceValues)
+    {
+        return SumDiceValues(diceValues);
+    }
+
+    // Ã„⁄ „ﬁ«œ?—  «”ùÂ«
+    private int SumDiceValues(int[] diceValues)
+    {
+        int sum = 0;
+        foreach (int value in diceValues)
+        {
+            sum += value;
+        }
+        return sum;
     }
 }
 
